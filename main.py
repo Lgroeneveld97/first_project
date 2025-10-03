963466f077d4e18bbc115ee922b29a513ad1188d
Lgroeneveld97 <lucasgroeneveld97@gmail.com>
2025-10-03 15:07:44 +0200

    add basic code

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-print("Hi!")
-test = [5]
-age = 34
-print(test[0])
-print(age)
-print("Hello, world!")
-global_var = "I am global"
+print("Hallo, wereld!")
+
+name = "Micheal"
+age = 20
+print(f"Mijn naam is {name} en ik ben {age} jaar oud.")
+
+age = age + 1
+print(f"Volgend jaar ben ik {age} jaar oud.")
+
+country = "Nederland"
+print(f"Ik kom uit {country}.")
+
+a = 10
+b = 3
+
+print(a + b)   # optellen → 13
+print(a - b)   # aftrekken → 7
+print(a * b)   # vermenigvuldigen → 30
+print(a / b)   # delen → 3.333...
+print(a // b)  # hele getallen delen → 3
+print(a % b)   # restwaarde → 1
+
+voornaam = "Lucas"
+achternaam = "Jansen"
+
+volledige_naam = voornaam + " " + achternaam
+print(volledige_naam)   # Lucas Jansen
+print("ha" * 20)   # hahaha... (20 keer)
+
+x = 5
+y = 10
+
+print(x < y)   # True
+print(x == y)  # False
+print(x != y)  # True
+
